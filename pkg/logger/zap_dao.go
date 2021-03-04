abbc6a0f7237d2d1ec967a727eb39c91f0d14ce6
Emad <emadghaffariii@gmail.com>
2021-03-04 19:38:19 +0000

    cleanup and refactor

--- pkg/logger/zap_dao.go
+++ pkg/logger/zap_dao.go
@@ -7,26 +7,34 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-type Log struct {
+// Log interface
+type Log interface {
+	Add(key string, field interface{}) *Log
+	Append(fields ...zapcore.Field) *Log
+	Level(level zapcore.Level) *Log
+	Development() *Log
+}
+
+type log struct {
 	logger *zap.Logger
 	fields []zapcore.Field
 	level  zapcore.Level
 }
 
 // Add new log
-func (l *Log) Add(key string, field interface{}) *Log {
+func (l *log) Add(key string, field interface{}) *log {
 	l.fields = append(l.fields, zap.Any(key, field))
 	return l
 }
 
 // Append new log
-func (l *Log) Append(fields ...zapcore.Field) *Log {
+func (l *log) Append(fields ...zapcore.Field) *log {
 	l.fields = append(l.fields, fields...)
 	return l
 }
 
 // Commit meth
-func (l *Log) Commit(message string) {
+func (l *log) Commit(message string) {
 	defer func() {
 		l.logger.Sync()
 		l.fields = nil
@@ -41,39 +49,34 @@ func (l *Log) Commit(message string) {
 		l.logger.Debug(message, l.fields...)
 	case zapcore.FatalLevel:
 		l.logger.Fatal(message, l.fields...)
-	case zapcore.PanicLevel:
-		l.logger.Panic(message, l.fields...)
 	default:
 		l.logger.Warn(message, l.fields...)
 	}
 }
 
 // Level of log
-func (l *Log) Level(level zapcore.Level) *Log {
+func (l log) Level(level zapcore.Level) *log {
 	l.level = level
-	return l
+	return &l
 }
 
 // Development method
-func (l *Log) Development() *Log {
-	var caller string
+func (l *log) Development() *log {
+	var caller string = ""
 
 	pc, _, line, ok := runtime.Caller(1)
 	details := runtime.FuncForPC(pc)
 	if ok && details != nil {
 		caller = details.Name()
 	}
-	runInfo := make(map[string]interface{})
 
-	runInfo["line"] = line
-	runInfo["caller"] = caller
-
-	l.Add("developer", runInfo)
+	l.Add("line", line)
+	l.Add("caller", caller)
 
 	return l
 }
 
 // Prepare new logger
-func Prepare(zap *zap.Logger) *Log {
-	return &Log{logger: zap}
+func Prepare(zap *zap.Logger) *log {
+	return &log{logger: zap}
 }
--- pkg/logger/zap_dto.go
+++ pkg/logger/zap_dto.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"go.uber.org/zap"
@@ -12,6 +13,8 @@ import (
 
 var (
 	logPath string
+	once    sync.Once
+	logger  *zap.Logger
 )
 
 // SetLogPath func
@@ -25,41 +28,45 @@ func SetLogPath(path string) {
 // GetZapLogger func
 // create new zap logger
 func GetZapLogger(debug bool) *zap.Logger {
-	w := zapcore.AddSync(
-		&lumberjack.Logger{
-			Filename:   fmt.Sprintf("%s/%s.log", logPath, time.Now().Local().Format("2006-01-02")),
-			MaxSize:    500, // megabytes
-			MaxBackups: 3,
-			MaxAge:     28,   //days
-			Compress:   true, // disabled by default
-		},
-	)
+	once.Do(func() {
+		w := zapcore.AddSync(
+			&lumberjack.Logger{
+				Filename:   fmt.Sprintf("%s/%s.log", logPath, time.Now().Local().Format("2006-01-02")),
+				MaxSize:    500, // megabytes
+				MaxBackups: 3,
+				MaxAge:     28,   //days
+				Compress:   true, // disabled by default
+			},
+		)
 
-	prodEC := zap.NewProductionEncoderConfig()
-	prodEC.EncodeTime = zapcore.RFC3339TimeEncoder
-	if debug {
+		prodEC := zap.NewProductionEncoderConfig()
+		prodEC.EncodeTime = zapcore.RFC3339TimeEncoder
+		if debug {
+			core := zapcore.NewTee(
+				zapcore.NewCore(
+					zapcore.NewJSONEncoder(prodEC),
+					w,
+					zap.InfoLevel,
+				),
+				zapcore.NewCore(
+					zapcore.NewConsoleEncoder(prodEC),
+					zapcore.AddSync(os.Stdout),
+					zapcore.DebugLevel,
+				),
+			)
+			logger = zap.New(core)
+		}
+
 		core := zapcore.NewTee(
 			zapcore.NewCore(
 				zapcore.NewJSONEncoder(prodEC),
 				w,
 				zap.InfoLevel,
 			),
-			zapcore.NewCore(
-				zapcore.NewConsoleEncoder(prodEC),
-				zapcore.AddSync(os.Stdout),
-				zapcore.DebugLevel,
-			),
 		)
-		return zap.New(core)
-	}
 
-	core := zapcore.NewTee(
-		zapcore.NewCore(
-			zapcore.NewJSONEncoder(prodEC),
-			w,
-			zap.InfoLevel,
-		),
-	)
+		logger = zap.New(core)
+	})
 
-	return zap.New(core)
+	return logger
 }
