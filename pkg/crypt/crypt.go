b13537af6e240ed6dec4027e238a7920eec3f410
Emad Ghaffari <emadghaffariii@gmail.com>
2021-05-18 09:45:37 +0000

    change method name

--- pkg/crypt/crypt.go
+++ pkg/crypt/crypt.go
@@ -12,12 +12,12 @@ type Micro interface{}
 
 type micro struct{}
 
-func Sign(password string) (string, error) {
+func Hash(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
 
-func Verify(password, hash string) bool {
+func CompareHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
