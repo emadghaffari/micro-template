a846c5f97f5ca10ca2ef07cdc611fa054e4d084b
Emad Ghaffari <emadghaffariii@gmail.com>
2021-05-19 13:04:36 +0000

    add username,password

--- config/etcd.go
+++ config/etcd.go
@@ -3,4 +3,6 @@ package config
 type ETCD struct {
 	Endpoints []string `json:"endpoints" yaml:"etcd.endpoints"`
 	WatchList []string `json:"watch_list" yaml:"etcd.watchList"`
+	Username  string   `json:"username" yaml:"etcd.username"`
+	Password  string   `json:"password" yaml:"etcd.password"`
 }
