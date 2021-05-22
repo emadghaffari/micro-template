cf8b030a89bf12afea6734cc0b121923e2461146
Emad Ghaffari <emadghaffariii@gmail.com>
2021-05-22 05:55:16 +0000

    cleanup and refactor

--- config/etcd.go
+++ config/etcd.go
@@ -2,7 +2,7 @@ package config
 
 type ETCD struct {
 	Endpoints []string `json:"endpoints" yaml:"etcd.endpoints"`
-	WatchList []string `json:"watch_list" yaml:"etcd.watchList"`
+	WatchList []string `json:"watch_list"`
 	Username  string   `json:"username" yaml:"etcd.username"`
 	Password  string   `json:"password" yaml:"etcd.password"`
 }
